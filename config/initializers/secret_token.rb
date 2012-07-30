746578e895d5abdfce292f562008ba08fe686abf
Rubik <wornaspam@hotmail.com>
2012-07-30 23:43:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '611d2b965fa6068277b925204caf8e9a33a6198353347af35693a316bddbab6e7a173902bb60befa521c10263bd9166ced90a871fdfa9693c7ab77b39f55dd60'
